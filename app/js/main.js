defdc1c752702df93ee812f966dd747f1316062d
evgKireev <bob1_k@mail.ru>
2022-06-09 13:34:15 +0300

    social

---
+++ app/js/main.js
@@ -0,0 +1,8 @@
+// $(function() {
+
+//   $('tabs__list-item').on('click', function(e) {
+//     e.preventDefault();
+//     $('tabs__list-item').removeClass('tabs__list-item--active');
+//     $(this).addClass('tabs__list-item--active');
+//   })
+// });
