79b1231c71165d061e3e1793ea65f0c1d4bbee5a
evgKireev <bob1_k@mail.ru>
2022-06-09 17:39:50 +0300

    add tabs

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,10 @@
-// $(function() {
+$(function() {
 
-//   $('tabs__list-item').on('click', function(e) {
-//     e.preventDefault();
-//     $('tabs__list-item').removeClass('tabs__list-item--active');
-//     $(this).addClass('tabs__list-item--active');
-//   })
-// });
+  $('.tabs__list-item').on('click', function(e) {
+    e.preventDefault();
+    $('.tabs__list-item').removeClass('tabs__list-item--active');
+    $(this).addClass('tabs__list-item--active');
+    $('.tabs__item').removeClass('tabs__item--active');
+    $($(this).attr('href')).addClass('tabs__item--active');
+  });
+});
